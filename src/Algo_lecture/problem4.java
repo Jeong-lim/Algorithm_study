860a5644ebe74cc66641403c6f5e10cac425edd5
Jeong-lim <wjdfla401@naver.com>
2023-07-31 09:37:58 +0900

    2023-07-28

--- src/Algo_lecture/problem4.java
+++ src/Algo_lecture/problem4.java
@@ -1,15 +1,28 @@
 package src.Algo_lecture;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class problem4 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        ArrayList<String> hint = new ArrayList<>(); // ArrayList 생성
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> hint = new ArrayList<>();
 
-        for(int i = 0; i <= 3; i++) {
-            hint.add(sc.nextLine());
-        } // [[123, 1, 1]
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            hint.add(Integer.parseInt(st.nextToken()));
+        }
+
+        for(int i = 0; i < hint.toArray().length; i++) {
+            System.out.println(hint.get(i));
+        }
+
+
+        // [[123, 1, 1]
     }
 }
